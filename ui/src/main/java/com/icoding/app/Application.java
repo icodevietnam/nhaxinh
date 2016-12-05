2c585dacc8b480c7f6353f267d53dcc5654abfc6
Phan Tuan Tu <icoding.active@gmail.com>
2016-12-05 08:29:46 +0700

    Init

---
+++ ui/src/main/java/com/icoding/app/Application.java
@@ -0,0 +1,13 @@
+package com.icoding.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.icoding.app.Application;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
